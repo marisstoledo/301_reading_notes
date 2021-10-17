819532ccb6cc282374b0bc81cba1b0efda355c5c
marisstoledo <toledoma@g.crookcounty.orggit config --global marisstoledogit config --global user.email toledoma@g.crookcounty.org>
2021-10-17 15:57:22 -0700

    finished lab

--- 301/arrow-functions/app.js
+++ 301/arrow-functions/app.js
@@ -197,12 +197,12 @@ Student.prototype.scope = function() {
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+ console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+//it will create a variable, and idenitfy the name 
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+//it will create a base,  
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// which you will be able to use different objects under the same class
